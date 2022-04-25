971b1d0919ea250fef32c6f846e36050eac374f1
corentinlevet <corentin.levet@epitech.eu>
2022-04-25 16:05:07 +0200

    [+] Adding header files for asm

---
+++ asm/include/asm.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** ASM_H_
+** File description:
+** Header file for asm
+*/
+
+#include "my.h"
+
+#ifndef ASM_H_
+    #define ASM_H_
+
+#endif /* ASM_H_ */
---
+++ asm/include/my.h
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2022
+** MY_H_
+** File description:
+** Header file for the library
+*/
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#ifndef MY_H_
+    #define MY_H_
+
+int my_strcmp(char const *s1, char const *s2);
+int my_strlen(char const *str);
+
+void my_putstr(char const *str);
+void my_putstr_error(char const *str);
+
+#endif /* !MY_H_ */
+
+#define UNDERLINE   "\033[04m"
+#define BOLD        "\033[01m"
+#define NORMAL      "\033[00m"
+#define RESET       "\033[0m"
+#define BLACK       "\033[30m"
+#define RED         "\033[31m"
+#define GREEN       "\033[32m"
+#define YELLOW      "\033[33m"
+#define BLUE        "\033[34m"
+#define MAGENTA     "\033[35m"
+#define CYAN        "\033[36m"
+#define WHITE       "\033[37m"
