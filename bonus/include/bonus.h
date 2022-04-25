49427a50855e37b7f4335063fe4e6e4e2305c710
corentinlevet <corentin.levet@epitech.eu>
2022-04-25 16:06:48 +0200

    [!] Adding bonus directory in case we do a visualizer

---
+++ bonus/include/bonus.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** BONUS_H_
+** File description:
+** Header file for bonus
+*/
+
+#include "my.h"
+
+#ifndef BONUS_H_
+    #define BONUS_H_
+
+
+#endif /* !BONUS_H_ */
---
+++ bonus/src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** MAIN
+** File description:
+** Main function for the bonus
+*/
+
+#include "../include/bonus.h"
+
+int main(int ac, char **av)
+{
+    return (0);
+}
