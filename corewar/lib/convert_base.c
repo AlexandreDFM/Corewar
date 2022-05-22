7b97b973a91c5f4a333a8f9d2b901966ac686457
corentinlevet <corentin.levet@epitech.eu>
2022-05-22 23:36:39 +0200

    [~] Fixing crash but adding C1

--- corewar/lib/convert_base.c
+++ corewar/lib/convert_base.c
@@ -36,12 +36,9 @@ char *hex_to_binary(char *hex)
     char *binary = malloc(sizeof(char) * (my_strlen(hex) * 4 + 1));
     my_memset(binary, 0, (my_strlen(hex) * 4 + 1));
     binary[(my_strlen(hex)) * 4] = '\0';
-    bool to_break = false;
     for (int i = 0; hex[i] != '\0'; i++) {
         for (j = 0; base[j] != '\0'; j++)
-            (hex[i] == base[j]) ? to_break = true : 0;
-        if (to_break == true)
-            break;
+            if (hex[i] == base[j]) break;
         for (; j != 0; k++) {
             binary[k] = j % 2 + 48;
             j /= 2;
